5d78a7e40900d300d71efa7481fbb7871beb8849
Han minji <jhn5052@naver.com>
2019-11-04 13:01:01 +0900

    boj algo

--- NandM9.cpp
+++ NandM9.cpp
@@ -3,10 +3,9 @@
 #include<algorithm>
 using namespace std;
 
-int N, M;
+int N,M;
 vector<int> v1, v2;
-bool flag[10001];
-int arr[10001] = { 0 };
+int visit[10001] { 0 };
 
 void recur(int num){
     if(num == M){
@@ -16,38 +15,44 @@ void recur(int num){
         cout << '\n';
         return;
     }
-    for(int i = 0; i < 10001; i++){
-        if(!flag[i] && arr[i] > 0){
-            flag[i] = true;
-            v2.push_back(i);
+    for(int i = 0; i < v1.size(); i++){
+        if(visit[v1[i]] > 0){
+            v2.push_back(v1[i]);
+            visit[v1[i]] -= 1;
             recur(num+1);
+            visit[v1[i]] += 1;
             v2.pop_back();
-            flag[i] = false;
         }
-
     }
 }
-            
-
 
 int main(){
     cin >> N >> M;
     int tmp;
-    
+    bool check = false;
     for(int i = 0; i < N; i++){
         cin >> tmp;
-        arr[tmp] += 1;
-    }
-    tmp = 0;
-    for(int i = 0; i < 10001; i++){
-        if(arr[i] > 0){   
-            flag[i] = true;
-            v2.push_back(i);
-            recur(1);
-            v2.pop_back();
-            flag[i] = false;
+        for(int j = 0; j < i; j++){
+            if(v1[j] == tmp){
+                check = true;
+            }
+        }
+        if(check == false){
+            v1.push_back(tmp);
+            visit[tmp]++;
+        }
+        else{
+            check = false;
+            visit[tmp]++;
         }
     }
+    sort(v1.begin(), v1.end());
+    for(int i = 0; i < v1.size(); i++){
+        v2.push_back(v1[i]);
+        visit[v1[i]] -= 1;
+        recur(1);
+        v2.pop_back();
+        visit[v1[i]] += 1;
+    }
     return 0;
-
 }
