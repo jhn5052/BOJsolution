da49b3b0b8aec61d33d9bc326912690c2d797329
jhn5052 <jhn5052@naver.com>
2020-03-18 17:46:07 +0900

    20200318

--- 14890.cpp
+++ 14890.cpp
@@ -7,10 +7,33 @@ int N, L, result = 0;
 int map[101][101];
 bool visit[101];
 
-int Find(int idx, bool s){
+int Row(int idx){
   int cnt = 1;
   for(int j = 0; j < N-1; j++){
-    int sub = (s == 1) ? map[idx][j+1] - map[idx][j] : map[j+1][idx] - map[j][idx];
+    int sub = map[idx][j+1] - map[idx][j];
+    if(sub == 0){
+      cnt += 1;
+    }
+    else if(sub == 1 && cnt >= L){
+      cnt = 1;
+    }
+    else if(sub == -1 && cnt >= 0){
+      cnt = -L + 1; //내려가는 경사로 
+    }
+    else{
+      return 0;
+    }
+  }
+  if(cnt >= 0){
+    result += 1;
+  }
+  return 0;
+}
+
+int Col(int idx){
+  int cnt = 1;
+  for(int j = 0; j < N-1; j++){
+    int sub = map[j+1][idx] - map[j][idx];
     if(sub == 0){
       cnt += 1;
     }
@@ -39,8 +62,8 @@ int main(){
     }
   }
   for(int i = 0; i < N; i++){
-    Find(i, 1);
-    Find(i, 0);
+    Row(i);
+    Col(i);
   }
   cout << result << '\n';
   return 0;
