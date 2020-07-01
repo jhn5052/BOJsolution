d0b53663d46c04843f292653fb9a25fc0e4f5812
jhn5052 <jhn5052@naver.com>
2020-07-01 16:15:55 +0900

    20200701

--- 2473.cpp
+++ 2473.cpp
@@ -1,65 +1,53 @@
 #include <iostream>
-#include <algorithm>
 #include <limits.h>
+#include <cmath>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
+typedef long long ll;
 int N;
-int L, R, M;
-long long arr[5001];
-int result[3];
-long long ans = INT_MAX;
+ll result = LONG_MAX;
+vector<ll> v;
+vector<ll> ans;
 
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    //input
     cin >> N;
+    int num;
     for(int i = 0; i < N; i++){
-        cin >> arr[i];
+        cin >> num;
+        v.push_back(num);
     }
-    sort(arr, arr + N);
     
-    for(auto x : arr){
-        cout << x << '\n';
-    }
-    //init
-    L = 0;
-    R = N-1;
-    M = 1;
-    while(L < R){
-        long long tmp = arr[L] + arr[R] + arr[M];
-        if(tmp == 0){
-            break;
-        }
-        else if(tmp < 0){
-            if(ans > tmp){
-                result[0] = arr[L];
-                result[1] = arr[M];
-                result[2] = arr[R];
+    sort(v.begin(), v.end());
+    for(int i = 0; i < N; i++){
+        for(int j = i+1; j < N; j++){
+            //구해야하는 값
+            int find = -(v[i] + v[j]);
+            int idx = lower_bound(v.begin()+j+1, v.end(), find) - v.begin();
+            for(int k = idx-2; k <= idx+2; k++){
+                if(k > j && k < N){
+                    ll tmp = v[i] + v[j] + v[k];
+                    if(abs(tmp) < result){
+                        result = abs(tmp);
+                        while(!ans.empty()){
+                            ans.pop_back();
+                        }
+                        ans.push_back(v[i]);
+                        ans.push_back(v[j]);
+                        ans.push_back(v[k]);
+                    }
+                }
             }
-
-            M++;
-        }
-        else if(tmp > 0){
-            if(ans > tmp){
-                result[0] = arr[L];
-                result[1] = arr[M];
-                result[2] = arr[R];
-            }
-            M--;
-        } 
-
-        if(M == L){
-            R--;
-        }
-        else if(M == R){
-            L++;
         }
     }
-
-    cout << result[0] << ' ' << result[1] << ' ' << result[2] << '\n';
+    sort(ans.begin(), ans.end());
+    for(auto x : ans){
+        cout << x << ' ';
+    }
     return 0;
 }
-
